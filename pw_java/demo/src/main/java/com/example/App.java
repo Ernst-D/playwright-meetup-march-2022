fba9a5b143c8f87f6b464340a674f78334638f70
Ernst-D <ernst.dzerevanko@gmail.com>
2022-03-28 00:52:40 +0400

    added page object model example in java

--- pw_java/demo/src/main/java/com/example/App.java
+++ pw_java/demo/src/main/java/com/example/App.java
@@ -2,15 +2,24 @@ package com.example;
 
 import com.microsoft.playwright.*;
 
+import models.gcp.MainPage;
+
 public class App {
   public static void main(String[] args) {
       try (Playwright playwright = Playwright.create()) {
         Browser browser = playwright.chromium().launch(
-          new BrowserType.LaunchOptions().setHeadless(false)
+          new BrowserType.LaunchOptions()
+          .setHeadless(false)
         );
         BrowserContext context = browser.newContext();
         Page page = context.newPage();
-        page.navigate("https://cloud.google.com");
+        MainPage gcpPage = new MainPage(page);
+        gcpPage.navigateToGCP();
+        gcpPage.searchForCalcluator().setUpCloudMachine();
+        gcpPage.getPage().pause();
+        
+        context.close();
+        browser.close();
     }
   }
 }
