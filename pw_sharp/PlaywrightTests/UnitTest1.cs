96b1bdce5adc63fc78a3baa42219ed8f73f62f84
Ernst-D <ernst.dzerevanko@gmail.com>
2022-03-28 23:06:39 +0400

    finished network interception

--- pw_sharp/PlaywrightTests/UnitTest1.cs
+++ pw_sharp/PlaywrightTests/UnitTest1.cs
@@ -1,7 +1,9 @@
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.Playwright;
 using NUnit.Framework;
+using Newtonsoft.Json;
 
 namespace PlaywrightTests;
 
@@ -9,43 +11,72 @@ public class Tests
 {
     public IBrowser _Browser { get; set;}
     public IPlaywright _Playwright { get; set;}
+    public IBrowserContext _Context { get; set; }
 
-    [SetUp]
+    public IPage _Page { get; set; }
+
+    [OneTimeSetUp]
     public async Task SetupAsync()
     {
         var playwright = await Playwright.CreateAsync();
         _Playwright = playwright;
 
-        var browser = await playwright.Chromium.LaunchAsync();
+        var browserLaunchOptions = new BrowserTypeLaunchOptions { 
+                Headless = false
+        };
+        var browser = await playwright.Chromium.LaunchAsync(browserLaunchOptions);
         _Browser = browser;
-        
+
+        _Context = await _Browser.NewContextAsync();
+
+        _Page = await _Context.NewPageAsync();
     }
 
     [TearDown]
     public async Task TearDownAsync(){
+        Console.WriteLine(await _Page.TitleAsync());
+        await _Page.PauseAsync();
+    }
+
+    [OneTimeTearDown]
+    public async Task OneTimeTearDownAsync(){
+        await _Context.CloseAsync();
         await _Browser.CloseAsync();
         _Playwright.Dispose();
     }
 
     [Test]
-    public async Task Test1Async()
-    {   
-        var gcpOptions = new BrowserNewContextOptions(_Playwright.Devices["iPhone 12 Pro"]){
-            Geolocation = new() { Longitude = 12.492507f, Latitude = 41.889938f },
-            Permissions = new[] { "geolocation" },
-            Locale = "de-DE"
-        };
-
-        var pwContext = await _Browser.NewContextAsync();
-        var gcpContext = await _Browser.NewContextAsync(gcpOptions);
-
-        var pwPage = await pwContext.NewPageAsync();
-        var gcpPage = await gcpContext.NewPageAsync();
-        
-        await pwPage.GotoAsync("https://playwright.dev/dotnet");
-        await pwPage.ScreenshotAsync(new PageScreenshotOptions { Path = "screenshot.pw.png" });
-        await gcpPage.GotoAsync("https://cloud.google.com");
-        Console.WriteLine(await gcpPage.TitleAsync());
-        await gcpPage.ScreenshotAsync(new PageScreenshotOptions { Path = "screenshot.gcp.png" });
+    public async Task InterceptContent()
+    {
+        await _Page.RouteAsync("**/*", async route =>{
+            if(route.Request.ResourceType == "stylesheet" ){
+                await route.AbortAsync();
+            }
+            else {
+                await route.ContinueAsync();
+            }
+        });
+        await _Page.GotoAsync("https://danube-webshop.herokuapp.com/");
+    }
+    [Test]
+    public async Task InterceptResponses()
+    { 
+        Object[] mockResponseObject = {new {
+            id =  1,
+            title = "How to Mock a Response",
+            author = "A. Friend",
+            genre = "business",
+            price = "0.00",
+            rating = "★★★★★",
+            stock = 65535
+        }};
+        var list = new List<object>(){mockResponseObject};
+        await _Page.RouteAsync("https://danube-webshop.herokuapp.com/api/books", async route =>{
+            await route.FulfillAsync(new RouteFulfillOptions {
+                ContentType = "application/json",
+                Body = JsonConvert.SerializeObject(mockResponseObject)
+            });
+        });
+        await _Page.GotoAsync("https://danube-webshop.herokuapp.com/");
     }
 }
