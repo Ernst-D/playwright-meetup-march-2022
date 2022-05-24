3d8f47765abac444ac2eb97903bda3a17bed2bce
Ernst-D <ernst.dzerevanko@gmail.com>
2022-05-25 02:00:31 +0400

    [java] refactored junit test

--- pw_java/demo/src/test/java/com/ernst/pw_example/AppTest.java
+++ pw_java/demo/src/test/java/com/ernst/pw_example/AppTest.java
@@ -1,20 +1,69 @@
 package com.ernst.pw_example;
 
-import static org.junit.Assert.assertTrue;
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserContext;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.FrameLocator;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+import org.junit.jupiter.api.*;
 
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+public class AppTest {
+  // Shared between all tests in this class.
+  static Playwright playwright;
+  static Browser browser;
+
+  // New instance for each test method.
+  BrowserContext context;
+  Page page;
+
+  static FrameLocator getWithinFrame(Page page){
+    FrameLocator firstFrame = page.frameLocator("devsite-iframe iframe");
+    FrameLocator secondFrame = firstFrame.frameLocator("#myFrame");
+    return secondFrame;
+  }
+
+  @BeforeAll
+  static void launchBrowser() {
+    playwright = Playwright.create();
+    browser = playwright.chromium().launch(new BrowserType.LaunchOptions()
+    .setHeadless(false)
+    .setSlowMo(1000));
+  }
+
+  @AfterAll
+  static void closeBrowser() {
+    playwright.close();
+  }
+
+  @BeforeEach
+  void createContextAndPage() {
+    context = browser.newContext();
+    page = context.newPage();
+  }
+
+  @AfterEach
+  void closeContext() {
+    context.close();
+  }
+
+  @Test
+  void shouldSearchWiki() {
+    page.navigate("https://cloud.google.com/products/calculator");
+    getWithinFrame(page)
+    .locator("[ng-model='listingCtrl.computeServer.os']").click();    
+    getWithinFrame(page).locator("[value='ubuntu-pro']").click();
+
+    getWithinFrame(page).locator("[ng-model='listingCtrl.computeServer.quantity']").fill("2");
+    getWithinFrame(page).locator("[ng-model='listingCtrl.computeServer.instance']").click();
+    getWithinFrame(page).locator("[value='CP-COMPUTEENGINE-VMIMAGE-E2-STANDARD-4']").click();
+    getWithinFrame(page)
+    .locator("[aria-label='ComputeEngineForm'] >> text=Add to Estimate").click();
+    assertTrue(
+      getWithinFrame(page).locator("[ng-if='appCtrl.CartData.hasItems()']").isVisible()
+    );
+    page.pause();
+  }
 }
