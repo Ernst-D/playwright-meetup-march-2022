c3f9b5f8d1e6b21420149bb0abec1c9cbd019b0b
Ernst-D <ernst.dzerevanko@gmail.com>
2022-05-24 23:54:17 +0400

    [java] added java example

--- pw_java/demo/src/main/java/com/ernst/pw_example/App.java
+++ pw_java/demo/src/main/java/com/ernst/pw_example/App.java
@@ -1,6 +1,8 @@
 package com.ernst.pw_example;
 
-import com.microsoft.playwright.*;;
+import com.microsoft.playwright.*;
+import com.microsoft.playwright.options.LoadState;
+import com.microsoft.playwright.options.WaitForSelectorState;;
 
 public class App 
 {
@@ -8,11 +10,32 @@ public class App
     {
         try (Playwright playwright = Playwright.create()) {
             Browser browser = playwright.chromium().launch(
-                new BrowserType.LaunchOptions().setHeadless(false)
+                new BrowserType.LaunchOptions()
+                .setHeadless(false)
+                .setSlowMo(200)
             );
             Page page = browser.newPage();
-            page.navigate("http://playwright.dev");
-            System.out.println(page.title());
+
+            page.onRequest(handler -> {
+                if(handler.url().endsWith("/graphql")){
+                    System.out.println(String.format("Operation name: %s \n", handler.postData()));
+                }
+            });
+            page.onConsoleMessage(handler -> {
+                System.out.println(String.format("%s | %s: %s \n", handler.type(),handler.location(),handler.text()));
+            });
+
+            page.navigate("http://localhost:3000/");
+            page.locator("input[name=\"username\"]").fill("Tavares_Barrows");
+            page.locator("input[name=\"password\"]").fill("s3cret");
+            page.locator("[data-test='signin-submit']").click();
+
+            page.waitForSelector("[data-test='transaction-list']", 
+                new Page.WaitForSelectorOptions().setState(WaitForSelectorState.VISIBLE)
+            );
+            page.waitForLoadState(
+                LoadState.NETWORKIDLE
+            );
           }
     }
 }
